95c21e4bbdf1715520fd04025016b7bce9c64ca1
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-04-18 22:36:00 +0200

    Rename variables and methods

--- app/src/main/java/com/marwa/Eltayeb/calculator/MainActivity.java
+++ app/src/main/java/com/marwa/Eltayeb/calculator/MainActivity.java
@@ -3,201 +3,194 @@ package com.marwa.Eltayeb.calculator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText txtShow;
-    double num1 = 0;
-    double num2 = 0;
+    EditText showOperation;
+    TextView displayResult;
+    double firstNumber = 0;
+    double secondNumber = 0;
     Double result;
-    char ope;
+    char mOperation;
 
-
-
-    private boolean readyToClear = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        //this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         setContentView(R.layout.activity_main);
 
 
-        txtShow = (EditText) findViewById(R.id.txtShow);
-        txtShow.requestFocus();
+        showOperation = (EditText) findViewById(R.id.showOperation);
+        showOperation.requestFocus();
+
+        displayResult = (TextView) findViewById(R.id.display_result);
 
     }
 
 
 
-    private void setAll(String s){
-        String all = txtShow.getText() + s;
-        txtShow.setText(all);
+    private void setAll(String number){
+        String all = showOperation.getText() + number;
+        showOperation.setText(all);
     }
 
-    private void setOpe(char operation){
-        String test = txtShow.getText().toString();
-        if (test.isEmpty())
-        {
-
+    private void setOperation(char operation) {
+        if (showOperation.getText().toString().isEmpty()) {
+            showOperation.setText("");
         }else {
-            num1 = Double.parseDouble(String.valueOf(txtShow.getText()));
-            txtShow.setText("");
-            ope = operation;
+            // Get the first Number and parse it into double
+            firstNumber = Double.parseDouble(String.valueOf(showOperation.getText()));
+            // Store specified operation into mOperation
+            showOperation.setText(null);
+            mOperation = operation;
         }
     }
 
 
+    /*
+    * Clear the result
+    */
     public void clear(View view) {
-        txtShow.setText("");
+        showOperation.setText("");
+        displayResult.setText("");
     }
 
     public void add(View view) {
-        setOpe('+');
+        setOperation('+');
     }
 
     public void subtract(View view) {
-        setOpe('-');
+        setOperation('-');
     }
 
     public void multiply(View view) {
-        setOpe('*');
+        setOperation('*');
     }
 
     public void divide(View view) {
-        setOpe('/');
+        setOperation('/');
     }
 
     public void percent(View view) {
-        setOpe('%');
+        setOperation('%');
     }
 
     public void backSpace(View view) {
-        if (!readyToClear) {
-                String txt = txtShow.getText().toString();
-                if (txt.length() > 0) {
-                    txt = txt.substring(0, txt.length() - 1);
-
-                    txtShow.setText(txt);
-                }
+        String txt = showOperation.getText().toString();
+        if (txt.length() > 0) {
+            txt = txt.substring(0, txt.length() - 1);
+            showOperation.setText(txt);
         }
     }
 
     public void zero(View view) {
         setAll("0");
-        txtShow.setEnabled(false);
     }
 
 
     public void one(View view) {
         setAll("1");
-        txtShow.setEnabled(false);
     }
 
     public void two(View view) {
         setAll("2");
-        txtShow.setEnabled(false);
     }
 
     public void three(View view) {
         setAll("3");
-        txtShow.setEnabled(false);
     }
 
 
     public void four(View view) {
         setAll("4");
-        txtShow.setEnabled(false);
     }
 
     public void five(View view) {
         setAll("5");
-        txtShow.setEnabled(false);
     }
 
     public void six(View view) {
         setAll("6");
-        txtShow.setEnabled(false);
     }
 
     public void seven(View view) {
         setAll("7");
-        txtShow.setEnabled(false);
     }
 
 
     public void eight(View view) {
         setAll("8");
-        txtShow.setEnabled(false);
     }
 
     public void nine(View view) {
         setAll("9");
-        txtShow.setEnabled(false);
     }
 
 
     public void dot(View view) {
         setAll(".");
-        txtShow.setEnabled(false);
     }
 
     public void positiveAndNegative(View view) {
-        String test2 = txtShow.getText().toString();
-        if(test2.isEmpty()){
-            //ignore
-        }else {
-            int num = Integer.parseInt(String.valueOf(txtShow.getText()));
+        if (showOperation.getText().toString().isEmpty()) {
+            showOperation.setText("");
+        } else {
+            int num = Integer.parseInt(String.valueOf(showOperation.getText()));
             num = num * (-1);
-            txtShow.setText(String.valueOf(num));
+            showOperation.setText(String.valueOf(num));
         }
     }
 
 
     public void equals(View view) {
         try {
-            num2 = Double.parseDouble(String.valueOf(txtShow.getText()));
-            switch (ope) {
+            secondNumber = Double.parseDouble(String.valueOf(showOperation.getText()));
+            switch (mOperation) {
                 case '+':
-                    result = num1 + num2;
+                    result = firstNumber + secondNumber;
                     break;
                 case '-':
-                    result = num1 - num2;
+                    result = firstNumber - secondNumber;
                     break;
                 case '*':
-                    result = num1 * num2;
+                    result = firstNumber * secondNumber;
                     break;
                 case '/':
-                    if (num2 == 0) //when denominator becomes zero
+                    if (secondNumber == 0) //when denominator becomes zero
                     {
                         Toast.makeText(this, "DIVISION NOT POSSIBLE", Toast.LENGTH_SHORT).show();
                         result = null;
-                        txtShow.setText("Error");
+                        displayResult.setText(getResources().getString(R.string.error));
                         break;
-                    }else{
-                        result = num1 / num2;
+                    } else {
+                        result = firstNumber / secondNumber;
                     }
                     break;
                 case '%':
-                    result = num1 % num2;
+                    result = firstNumber % secondNumber;
                     break;
             }
+
             Integer intResult = result.intValue();
-            double testResult = result/intResult;
-            if(testResult!=1) {
-                txtShow.setText(result.toString());
-            }else{
-                txtShow.setText(intResult.toString());
+            double testResult = result / intResult;
+            ///String updatedResult = (int)firstNumber + getOperation() + (int)secondNumber;
+            if (testResult != 1) {
+                displayResult.setText(getString(R.string.equals) + String.valueOf(result));
+            } else {
+                displayResult.setText(getString(R.string.equals) + String.valueOf(intResult));
             }
+            ///showOperation.setText(updatedResult);
+            ///showAll();
 
-        }catch(Exception e){
-            //ignore
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        ///Toast.makeText(this, result +"", Toast.LENGTH_SHORT).show();
     }
-
 
 
 
